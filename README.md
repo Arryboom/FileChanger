deccb664709026f27cf4f2ecb4dfa979f1943a20
Noah Wood <woodysfoundry@gmail.com>
2018-09-18 13:25:50 -0400

    Added files

--- README.md
+++ README.md
@@ -11,3 +11,11 @@ No. I planned on releasing this with a dead-man's switch, but if I did that I mi
 Partially for the fun of it, but also to point out that computer forensic evidence is flawed. It may take hours from the time a laptop is seized until the time the disk drive is imagedl during that time the laptop will go from a "beat" police officer to another to a detective who will retrieve it from the evidence room, likely open it up to see if he can get in before calling a forensics investigator which could take days to weeks depending on how important the case is. That is a very, very long time for a computer to be sitting, and we can't rely on evidence that has the potential to delete or change itself. Not only that, but an individual could use a program similar to this one to make it appear as if a file is OLDER than it really is by modifying it to change the system date/time and thereby use it to fabricate evidence.
 
 In short, I made it because Computer Forensics is flawed in this regard and it needed to be pointed out.
+
+# How To Use:
+It's pretty simple, just type
+    python3 forensicfucker.py
+into a terminal then check the results in SmokingGunTimes
+
+# Why isn't it self-destructing?
+It's set to not self-destruct by default, if you open up the .py file and change the setting "self_destruct = True" it will automagically erase the python script, the the SmokingGunTimes file, and the list of files then it will kill itself ending the process. 
---
+++ forensicfucker.py
@@ -0,0 +1,61 @@
+import subprocess
+
+def selfDestruct():
+    from sys import argv
+    process = subprocess.Popen(["shred", "-u", argv[0]])
+    process.wait()
+    quit()
+    
+def statCheck(fname, i):
+    f = open("SmokingGunTimes","a+")
+    stat = subprocess.Popen(["stat",fname], stdout=subprocess.PIPE)
+    res = stat.communicate()[0]
+    if i == 0:
+        f.write("\nOld STAT\n")
+    elif i ==1:
+        f.write("\nNew STAT\n")
+    f.write(str(res))
+    f.close()
+    
+def delFile(file):
+    process = subprocess.Popen(["shred", "-u", file])
+    process.wait()
+    
+def changer(fname):
+    i = 0
+    nname =  fname + 'something_random'
+    statCheck(fname,i)
+    i=+1
+    p1 = subprocess.Popen(["cp", fname, nname])
+    p1.wait()
+    delFile(fname)
+    p3 = subprocess.Popen(["mv", nname, fname])
+    p3.wait()
+    p4 = subprocess.Popen(["touch", "-a", "-m", fname])
+    p4.wait()
+    delFile(nname)
+    statCheck(fname,i)
+
+def useFile(file, output_file, self_destruct):
+    with open(file, "r") as f:
+        j =0
+        for line in f:
+            print(line)
+            j = +1
+            changer(line)
+        f.close()
+    if self_destruct == True:
+        delFile(file)
+        delFile(output_file)
+        selfDestruct()
+    else:
+        quit()
+
+#################################
+#         Settings              #
+files_list = 'files.list'       #
+output_file = 'SmokingGunTimes' #
+self_destruct = False           #
+#################################
+if __name__ == '__main__':
+    useFile(files_list, output_file, self_destruct)
